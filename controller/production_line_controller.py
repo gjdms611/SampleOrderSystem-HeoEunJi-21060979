08abe724ca6a8565a493afe059db2467c938e73d
gjdms611 <gjdms611@gmail.com>
2026-07-15 16:54:55 +0900

    feat(phase25-B): ProductionLineController.tick() 구현 GREEN
    경과시간 기반 목표생산량 계산 후 produce_unit 반복호출,
    완료된 job은 order_controller.complete_production으로 Order 상태 반영

--- controller/production_line_controller.py
+++ controller/production_line_controller.py
@@ -1,3 +1,7 @@
+import math
+import time
+
+
 class ProductionLineController:
     def __init__(self, queue):
         self.queue = queue
@@ -7,3 +11,18 @@ class ProductionLineController:
 
     def waiting_jobs(self):
         return list(self.queue.waiting)
+
+    def tick(self, inventory_repo, order_controller):
+        confirmed_jobs = []
+        for line_index, job in enumerate(self.queue.lines):
+            if job is None:
+                continue
+            per_unit_time = job.total_production_time / job.actual_qty
+            elapsed = time.time() - job.started_at
+            target_qty = min(job.actual_qty, math.floor(elapsed / per_unit_time))
+            while job.produced_qty < target_qty:
+                inventory = inventory_repo.find_by_sample_id(job.sample_id)
+                confirmed_jobs.extend(self.queue.produce_unit(line_index, inventory))
+                inventory_repo.save(inventory)
+
+        return order_controller.complete_production(confirmed_jobs)
